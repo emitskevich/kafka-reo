470d5782390db6ff0fde7a24b59e4edb20f7b4b3
emitskevich <evg.mitskevich@gmail.com>
2023-01-16 15:36:50 -0500

    Conf: update lib versions

--- replicator/build.gradle.kts
+++ replicator/build.gradle.kts
@@ -13,18 +13,18 @@ repositories {
 
 dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.5")
-    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("org.jetbrains:annotations:23.1.0")
     implementation("com.github.pcj:google-options:1.0.0")
     implementation("org.yaml:snakeyaml:1.33")
-    implementation("com.google.protobuf:protobuf-java:3.21.9")
+    implementation("com.google.protobuf:protobuf-java:3.21.12")
     implementation("org.apache.kafka:kafka-streams:3.3.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testImplementation("org.mockito:mockito-core:4.8.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.mockito:mockito-core:4.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.11.0")
     testImplementation("org.apache.kafka:kafka-streams-test-utils:3.3.1")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
 application {
@@ -33,7 +33,7 @@ application {
 
 tasks {
     protobuf {
-        protoc { artifact = "com.google.protobuf:protoc:3.21.9" }
+        protoc { artifact = "com.google.protobuf:protoc:3.21.12" }
     }
 }
 
